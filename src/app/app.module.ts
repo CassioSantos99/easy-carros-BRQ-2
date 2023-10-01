61c20b96ca4458d0fe92620b689802e6bd0bb2d8
Cássio Santos <hsantos.cassio@gmail.com>
2023-10-01 11:09:25 -0300

    mvp/brq2-v1.0.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
+import 'bootstrap';
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { CardsOptionsComponent } from './shared/cards-options/cards-options.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CardsOptionsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/shared/cards-options/cards-options.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cards-options',
+  templateUrl: './cards-options.component.html',
+  styleUrls: ['./cards-options.component.scss']
+})
+export class CardsOptionsComponent {
+
+}
